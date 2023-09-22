a822cab42121fb22fcf5a1630608ba8a94ca121f
Marco Galvagni <marco.galvagni@elmec.it>
2023-09-22 09:15:16 +0200

    Update ransomfeed parser

--- ransomfeed.py
+++ ransomfeed.py
@@ -1,17 +1,25 @@
 import feedparser
+import re
 
-def update():
+def parse_ransomfeed():
     ransomfeed_url = "https://ransomfeed.it/rss-complete.php"
 
-    ransomfeed_parsed = feedparser.parse(ransomfeed_url)
+    ransomfeed_parsed = feedparser.parse(ransomfeed_url)   
 
     for e in ransomfeed_parsed['entries']:
-        if "The target comes from <b>Italy</b>" in e['summary']:
-            print(f"{e['title']} - {e['tags'][0]['term']}")
+        country_match = re.search(r'The target comes from <b>(.*?)</b>', e['summary_detail']['value'])
+        hash_match = re.search(r'We identify this attack with following <b>hash code</b>: <i>(.*?)</i>', e['summary_detail']['value'])
+        website_match = re.search(r'Target victim <b>website</b>: <i>(.*?)</i>', e['summary_detail']['value'])
 
-
-def main():
-    update()
+        result = {
+            "hash": hash_match.group(1) if hash_match else "",
+            "victim": f"{e['title']}",
+            "country": country_match.group(1) if country_match else "",
+            "website": website_match.group(1) if website_match else "",
+            "group": f"{e['tags'][0]['term']}"
+        }
+        print(result)
+    
 
 if __name__ == '__main__':
-    main()
+    parse_ransomfeed()
