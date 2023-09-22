1b8ee4b2b11f1944114ab8843555ccdda9dfa578
SRT88 <paolo96.ferro@gmail.com>
2023-09-22 14:40:16 +0200

    Update Bot

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@ import telegram
 import asyncio
 import logging
 import os
+import pymongo
 
-TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_API_KEY = os.environ.get('TELEGRAM_API_KEY')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
@@ -13,10 +14,34 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-async def send_message():
+async def send_message(): 
+    print(TELEGRAM_API_KEY)
     telegram_bot = telegram.Bot(token=TELEGRAM_API_KEY)
-    message_text = 'TEST'
-    await telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message_text)
+    mongo_client = pymongo.MongoClient(f"mongodb://localhost:27017")
+    db = mongo_client.ransomware
+    dataDB = db['data']
+
+    query = {'notified': False}
+    documents = dataDB.find(query)
+
+    for document in documents:
+        if document:
+            title = document.get('title', '')
+            link = document.get('link', '')
+            published_date = document.get('published_date', '')
+            #summary = document.get('summary', '')
+            message = f"Titolo: {title}\n" \
+              f"Link: {link}\n" \
+              f"Data di pubblicazione: {published_date}\n" 
+            
+            # Aggiornare stato di notifica
+            dataDB.update_one({'title': title}, {'$set': {'notified': True}})
+
+            await telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+
+
+    #message_text = document
+    
 
 def main():
     loop = asyncio.get_event_loop()
