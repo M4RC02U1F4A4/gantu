b0a356ce047e8d6c0d6b3dedddd343f6673a9a17
Marco Galvagni <marco.galvagni@elmec.it>
2023-09-22 17:55:31 +0200

    Some changes

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ import pymongo
 
 TELEGRAM_API_KEY = os.environ.get('TELEGRAM_API_KEY')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+LOGLEVEL = os.getenv('LOGLEVEL').upper()
 
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level="WARNING")
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-# logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
 async def send_message(): 
-    print(TELEGRAM_API_KEY)
     telegram_bot = telegram.Bot(token=TELEGRAM_API_KEY)
     mongo_client = pymongo.MongoClient(f"mongodb://localhost:27017")
     db = mongo_client.ransomware
@@ -26,21 +26,17 @@ async def send_message():
 
     for document in documents:
         if document:
-            title = document.get('title', '')
-            link = document.get('link', '')
-            published_date = document.get('published_date', '')
-            #summary = document.get('summary', '')
-            message = f"Titolo: {title}\n" \
-              f"Link: {link}\n" \
-              f"Data di pubblicazione: {published_date}\n" 
+            id = document.get('_id', '')
+            title = document.get('victim', '')
+            website = document.get('website', '')
+            published = document.get('published', '')
+            group = document.get('group', '')
+
+            message = f"*{title}*\n\nWebsite: {website}\nCompromised by: {group}\n\n{published}" 
             
-            # Aggiornare stato di notifica
-            dataDB.update_one({'title': title}, {'$set': {'notified': True}})
+            dataDB.update_one({'_id': id}, {'$set': {'notified': True}})
 
-            await telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-
-
-    #message_text = document
+            await telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message, parse_mode="Markdown")
     
 
 def main():
