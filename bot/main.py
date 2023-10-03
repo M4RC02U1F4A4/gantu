d1b578ccac339101a29524086f13456d30871f0c
Marco Galvagni <marco.galvagni@elmec.it>
2023-10-03 18:48:55 +0200

    Healthchecks

--- bot/main.py
+++ bot/main.py
@@ -3,11 +3,13 @@ import asyncio
 import logging
 import os
 import pymongo
+import requests
 
 TELEGRAM_API_KEY = os.environ.get('TELEGRAM_API_KEY')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 LOGLEVEL = os.getenv('LOGLEVEL').upper()
 MONGODBSTRING = os.getenv('MONGODBSTRING')
+BOT_HEALTHCHECKS_ID = os.getenv('BOT_HEALTHCHECKS_ID')
 
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level="INFO")
@@ -37,10 +39,18 @@ async def send_message():
             message = f"*{title}*\n\nWebsite: {website}\nCompromised by: {group}\nCountry: {country}\n\n{published}" 
             logging.info(document)
             dataDB.update_one({'_id': id}, {'$set': {'notified': True}})
+            try:
+                requests.get(f"https://hc-ping.com/{BOT_HEALTHCHECKS_ID}", timeout=10)
+            except requests.RequestException as e:
+                logging.error("Ping failed: %s" % e)
             await telegram_bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message, parse_mode="Markdown")
     
 
 def main():
+    try:
+        requests.get(f"https://hc-ping.com/{BOT_HEALTHCHECKS_ID}/start", timeout=10)
+    except requests.RequestException as e:
+        logging.error("Ping failed: %s" % e)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_message())
     loop.close()
--- updater/main.py
+++ updater/main.py
@@ -2,10 +2,17 @@ import pymongo
 import ransomfeed
 import logging
 import os
+import requests
 
 LOGLEVEL = os.getenv('LOGLEVEL').upper()
 MONGODBSTRING = os.getenv('MONGODBSTRING')
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=LOGLEVEL)
+UPDATER_HEALTHCHECKS_ID = os.getenv('UPDATER_HEALTHCHECKS_ID')
+
+try:
+    requests.get(f"https://hc-ping.com/{UPDATER_HEALTHCHECKS_ID}/start", timeout=10)
+except requests.RequestException as e:
+    logging.error("Ping failed: %s" % e)
 
 mongo_client = pymongo.MongoClient(MONGODBSTRING)
 db = mongo_client.ransomware
@@ -23,3 +30,8 @@ for r in ransomfeed.parse_ransomfeed(notified):
     except Exception as e:
         dataDB.update_one({"_id":r['_id']}, {"$set":{"victim":r['victim'], "country":r['country'], "website":r['website'], "group":r['group'], "published":r['published']}})
         logging.warn(e, exc_info=False)
+
+try:
+    requests.get(f"https://hc-ping.com/{UPDATER_HEALTHCHECKS_ID}", timeout=10)
+except requests.RequestException as e:
+    logging.error("Ping failed: %s" % e)
